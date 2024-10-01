2ee35b2d3af64751f212b8a7e5099924d988f18a
kerenelbaz <132551849+kerenelbaz@users.noreply.github.com>
2024-10-01 12:38:56 +0300

    Update apies.js

--- scripts/apies.js
+++ scripts/apies.js
@@ -1,6 +1,7 @@
 // lizi
 //const port = `https://localhost:7062` 
 //idan
-//const port = `https://localhost:7070`
+const port = `https://localhost:7070`
+
 //keren
-const port = `https://proj.ruppin.ac.il/cgroup11/test2/tar1`
+// const port = `https://proj.ruppin.ac.il/cgroup11/test2/tar1`
